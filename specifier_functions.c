79b43a0c47d1bade588351ca313ae59c9dda1ec6
Benyarko1 <145772796+Benyarko1@users.noreply.github.com>
2023-11-13 14:23:08 +0000

    Update specifier_functions.c
    Try this locally

--- specifier_functions.c
+++ specifier_functions.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+/*PRINT C*/
+
 /**
   * print_char - Prints a character to the standard output
   * @args: Input arguments
@@ -7,7 +9,7 @@
   * Return: Lenght of characters
   */
 
-int print_char(va_list args)
+int print_char(va_list args, int flags, int width, int precision)
 {
 	char output = va_arg(args, int);
 
@@ -15,22 +17,71 @@ int print_char(va_list args)
 	return (1);
 }
 
+/*PRINT S*/
 
 /**
   * print_string - Prints a string
-  * @args: A list of variadic arguments
+  * @args: Input arguments
   *
+  * @precision: Precision specification
+  * @flags: Calculates active flags
+  * @width: get width.
   * Return: Length of string
   */
 
-int print_string(va_list args)
+int print_string(va_list args, int flags, int width, int precision)
 {
 	char *arg = va_arg(args, char *);
 	int len_of_str = 0;
 
-	while (arg[len_of_str] != '\0')
-		len_of_str++;
+	if (str == NULL)
+	{
+		str = "(null)";
+		if (precision >= 6)
+			str = "      ";
+	}
 
-	write(1, arg, len_of_str);
-	return (len_of_str);
+	while (len_of_str != '\0')
+		length++;
+
+	if (precision >= 0 && precision < length)
+		length = precision;
+
+	if (width > length)
+	{
+	if (flags & F_MINUS)
+	{
+		write(1, &str[0], length);
+		for (i = width - length; i > 0; i--)
+		write(1, " ", 1);
+		return (width);
+	}
+		else
+		{
+		for (i = width - length; i > 0; i--)
+		write(1, " ", 1);
+		write(1, &str[0], length);
+		return (width);
+		}
+	}
+}
+
+/*PRINT PERCENTAGE SIGN*/
+
+/**
+  * print_percent - Prints a percentage sign
+  * @args: Input arguments
+  *
+  * @precision: Precision specification
+  * @flags: Calculates active flags
+  * @width: get width.
+  * Return: Number of chars printed
+  */
+
+int print_percent(va_list args, int flags, int width, int precision)
+{
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	return (write(1, "%%", 1));
 }
